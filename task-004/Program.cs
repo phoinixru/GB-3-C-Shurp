b68d6278832a520661a2f89f3e8d4da263febbfc
phoinixru <phoinixru@gmail.com>
2022-08-24 19:23:19 +0200

    task-004

--- task-004/Program.cs
+++ task-004/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числаи
+и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Enter thre numbers: a b c");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a > b ? a : b;
+max = c > max ? c : max;
+
+Console.WriteLine($"Max = {max}");
