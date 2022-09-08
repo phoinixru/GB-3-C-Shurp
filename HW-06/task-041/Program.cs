00f02c7ce5084cb2fe852bcdcc7073b7fa4a52fd
phoinixru <phoinixru@gmail.com>
2022-09-08 14:44:59 +0200

    HW-06

---
+++ HW-06/task-041/Program.cs
@@ -0,0 +1,68 @@
+﻿/* Задача 41:
+Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+var Log = (string text) => Console.WriteLine(text);
+var ArrayToString = (int[] array) => "[" + String.Join(", ", array) + "]";
+
+
+int m = 5;
+int[] userInput = ReadArray(m);
+
+if (userInput.Length != m) {
+    Log("Wrong input");
+    return;
+}
+
+int positiveCount = CountPositive(userInput);
+
+Log(
+    $"There {(positiveCount == 1 ? "is" : "are")} {positiveCount} positive number{(positiveCount == 1 ? "" : "s")} in array: \n" +
+    ArrayToString(userInput)
+);
+
+
+
+int CountPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        count += array[i] > 0 ? 1 : 0;
+    }
+    return count;
+}
+
+int[] ReadArray(int len)
+{
+    Console.WriteLine($"Input an array of {len} numbers using ',' as a separator:");
+    int[] empty = new int[0];
+
+    var input = Console.ReadLine();
+    if (input == null || len == 0)
+    {
+        return empty;
+    }
+
+    int[] result = new int[len];
+    string[] chunks = input.Split(',');
+
+    if (chunks.Length != len)
+    {
+        return empty;
+    }
+
+    for (int i = 0; i < chunks.Length; i++)
+    {
+        bool isNumber = int.TryParse(chunks[i], out int n);
+        if (!isNumber)
+        {
+            return empty;
+        }
+        result[i] = n;
+    }
+    return result;
+}
---
+++ HW-06/task-043/Program.cs
@@ -0,0 +1,71 @@
+﻿/* Задача 43:
+Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+var Log = (string text) => Console.WriteLine(text);
+var ArrayToString = (int[] array) => "[" + String.Join(", ", array) + "]";
+var PointToString = (double[] point) => "(" + String.Join("; ", point) + ")";
+
+int[] userInput = InputLines();
+if (userInput.Length != 4)
+{
+    Log("Wrong input");
+    return;
+}
+else
+{
+    Log($"User input: {ArrayToString(userInput)}");
+}
+
+int b1 = userInput[0];
+int k1 = userInput[1];
+int b2 = userInput[2];
+int k2 = userInput[3];
+bool isParallelLines = IsParallelLines(k1, k2);
+
+if (isParallelLines)
+{
+    Log("These are parallel lines");
+    return;
+}
+
+double[] crossPoint = FindIntersection(k1: k1, b1: b1, k2: k2, b2: b2);
+Log($"Lines cross at: {PointToString(crossPoint)}");
+
+int[] InputLines()
+{
+    string[] names = { "k1", "b1", "k2", "b2" };
+    int len = names.Length;
+    int[] result = new int[len];
+
+    for (int i = 0; i < len; i++)
+    {
+        Log($"Enter {names[i]}:");
+
+        bool isNumber = int.TryParse(Console.ReadLine(), out int n);
+        if (!isNumber)
+        {
+            return new int[0];
+        }
+        result[i] = n;
+    }
+    return result;
+}
+
+bool IsParallelLines(int k1, int k2)
+{
+    return k1 == k2;
+}
+
+double[] FindIntersection(int b1, int k1, int b2, int k2)
+{
+
+    double x = (double) (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    double[] point = { x, y };
+
+    return point;
+}
